4ab88763638a4bc56bfc214226be1a31b7dd7de3
vosxodd <labuzovalexey@gmail.com>
2022-12-29 01:32:35 +0700

    сделан CategoryAndPathsDto и методы работы с ним.

---
+++ src/main/java/rest/dto/CategoryAndPathsDto.java
@@ -0,0 +1,15 @@
+package rest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryAndPathsDto {
+    private String category;
+    private List<String> listOfPaths;
+}
