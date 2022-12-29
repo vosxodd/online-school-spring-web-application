b1069ff6ca2c2997cd6471f48194872696e178e5
Waskoff <nekton0@mail.ru>
2022-12-29 14:41:44 +0700

    Отредактировал LogPageController.java (2)

--- src/main/java/rest/controllers/LogPageController.java
+++ src/main/java/rest/controllers/LogPageController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 import rest.persistence.entity.Person;
 import rest.persistence.repository.PersonRepository;
 
@@ -24,13 +25,13 @@ public class LogPageController {
     }
 
     @PostMapping("/logpage")
-    public ModelAndView logPerson(Person person) {
+    public ModelAndView logPerson(Person person, ModelAndView model) {
         List<Person> all_persons = personRepository.findAllPerson();
         ModelAndView modelAndView = new ModelAndView();
         for (Person iterable_person: all_persons) {
             if (iterable_person.getEmail().equals(person.getEmail())) {
-                modelAndView.setViewName("mainpage");
-                return modelAndView;
+                model.setView(new RedirectView("/"));
+                return model;
             }
         }
         modelAndView.setViewName("logpage");
